89f729996a168fdd551d8555a4dead0f7ef00838
Andrew Sasamori <andrewsasamori@gmail.com>
2024-01-27 02:31:25 +0000

    I think this is entirely wrong but I have to start somewhere. Added some code form the doforke.c snipet from Listing 4.2 from the textbook

--- myshell_parser.c
+++ myshell_parser.c
@@ -3,7 +3,14 @@
 
 struct pipeline *pipeline_build(const char *command_line)
 {
-	// TODO: Implement this function
+	// TODO: Implement this functio
+	pid_t, cpid;
+	int status = 0;
+	cpid = fork();
+	if (cpid < 0){
+		perror("fork failed\n");
+		exit(-1);
+	}
 	return NULL;
 }
 
